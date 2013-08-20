d3b9dd22e69e978669837125173d97bdbfa86b8e
Colin Bookman <cobookman@gmail.com>
2013-08-20 13:11:12 -0400

    added jsonp support

--- controllers/api/core.js
+++ controllers/api/core.js
@@ -3,30 +3,30 @@ var api = require('../../api/core_api').init();
 
 exports.areaC = function(req, res){
     api.areaC(function(data) {
-        res.json(data);
+        res.jsonp(data);
     });
 }
 
 exports.areaE = function(req, res) {
    api.areaE(function(data) {
-        res.json(data);
+        res.jsonp(data);
     });
 }
 
 exports.globalPerspectives = function(req, res) {
 	api.globalPerspectives(function(data) {
-		res.json(data);
+		res.jsonp(data);
 	});
 }
 
 exports.usPerspectives = function(req, res) {
 	api.usPerspectives(function(data) {
-		res.json(data);
+		res.jsonp(data);
 	});
 }
 
 exports.ethics = function(req, res) {
 	api.ethics(function(data) {
-		res.json(data);
+		res.jsonp(data);
 	});
 }
